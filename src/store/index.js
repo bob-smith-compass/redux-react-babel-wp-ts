b567c65cb44d7bf25b629c70cf10129ac679881c
David <david.shams@gmail.com>
2019-08-17 16:02:40 -0500

    Redux - add to your application

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,5 @@
 import { createStore } from 'redux';
 import { defaultState } from '../server/defaultState';
-// import { defaultState } from '../server/defautState';
 
 export const store = createStore(
     function (state = defaultState, action) {
